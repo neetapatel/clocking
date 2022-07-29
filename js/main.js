976bebc1a5746fd3b162010681c9eb58c0c88e70
neetapatel <npfluter@gmail.com>
2022-07-29 10:53:50 +0100

    speed feature

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 var totalSecs = 0;
+var mins = 0;
+var secs = 0;
+var interval_normal, interval_2x;
 
 function setTime() { // onclick
-  var intervalID = setInterval(increment, 1000);
+  clearInterval(interval_normal);
+  clearInterval(interval_2x);
+  interval_normal = setInterval(increment, 1000);
+}
+
+function clockOut() {
+  clearInterval(interval_normal);
+  clearInterval(interval_2x);
+  interval_2x = setInterval(increment, 500);
 }
 
 function increment() {
   totalSecs++;
-  var mins = Math.floor(totalSecs / 60);
+  mins = Math.floor(totalSecs / 60);
   if (mins <= 9) {
     mins = "0" + mins;
   }
-  var secs = Math.floor(totalSecs) % 60;
+  secs = Math.floor(totalSecs) % 60;
   if (secs <= 9) { 
     secs = "0" + secs; 
   }
   document.getElementById("txt").innerHTML = mins + ":" + secs;
-  checkTime(mins, secs);
-}
-
-function checkTime(m, s) {
-    
 }
